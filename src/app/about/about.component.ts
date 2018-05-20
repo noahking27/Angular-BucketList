17472d93bc302258bfcff2e6136aa02c18d0defd
Noah King <noahking@Noahs-MacBook-Pro.local>
2018-05-20 17:02:26 -0400

    push bucket list angular app

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+
+  constructor(private router: Router) {
+   }
+
+  ngOnInit() {
+  }
+
+  sendMeHome(){
+    this.router.navigate(['']);
+  }
+
+}
